0e6f6af7fc4f37456f3a296db4b47db1185b961f
Aditya Purkar <aspurkar@gmail.com>
2017-05-13 11:31:46 -0700

    Final Commit

---
+++ AuthFilter/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using AuthFilter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AuthFilter.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [AllowAnonymous]
+        public ActionResult login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult login(LoginModel model,string returnUrl)
+        {
+            if(FormsAuthentication.Authenticate(model.Username,model.Password))
+            {
+                FormsAuthentication.SetAuthCookie(model.Username, false);
+                return Redirect(!string.IsNullOrEmpty(returnUrl) ? returnUrl : Url.Action("index", "home"));
+            }
+            else
+            {
+                ViewBag.Error = "Invalid creds";
+                return View();
+
+            }
+        }
+    }
+}
---
+++ AuthFilter/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AuthFilter.Models
+{
+    public class LoginModel
+    {
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Username { get; set; }
+    }
+}
